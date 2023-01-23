a8adb9bd24aea5399e5e3d3bd16368f9be7dad20
quboqin <quboqin0710@gmail.com>
2023-01-23 12:38:57 +0800

    get content from message

--- src/bot.js
+++ src/bot.js
@@ -5,15 +5,25 @@ import discord from 'discord.js'
 const { Client, GatewayIntentBits } = discord
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.MessageContent,
+  ],
 })
 
 client.once('ready', () => {
   console.log(`bot is online`)
 })
 
-client.on('message', async (message) => {
-  console.log(message)
+// https://stackoverflow.com/questions/73036854/message-content-doesnt-have-any-value-in-discord-js
+client.on('messageCreate', async (message) => {
+  console.log(message.content)
   if (message.content === 'ping') {
     message.reply('pong')
   }
