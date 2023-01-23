abbcefb1e921595bd775088caf5eeeecabae18ba
quboqin <quboqin0710@gmail.com>
2023-01-23 16:17:00 +0800

    send message to user and channel

--- src/bot.js
+++ src/bot.js
@@ -7,6 +7,9 @@ const { Client, GatewayIntentBits } = discord
 import fs from 'fs'
 import fetch from 'node-fetch'
 
+// add midjourney to my server
+// https://www.followchain.org/add-midjourney-bot-discord-server/
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -20,8 +23,20 @@ const client = new Client({
   ],
 })
 
-client.once('ready', () => {
+// https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
+client.once('ready', async () => {
   console.log(`bot is online`)
+  const user = await client.users.fetch('450851527364575244')
+  console.log(`user = ${user}`)
+  if (user) {
+    user.send('hello')
+  }
+
+  const channel = await client.channels.fetch('1066898073223172118')
+  console.log(`channel = ${channel}`)
+  if (channel) {
+    channel.send('hello')
+  }
 })
 
 // https://stackoverflow.com/questions/73036854/message-content-doesnt-have-any-value-in-discord-js
@@ -34,7 +49,7 @@ client.on('messageCreate', async (message) => {
   if (attachment && /\.(png|jpe?g|svg)$/.test(attachment.url)) {
     console.log(attachment.url)
     fetch(attachment.url)
-      .then((res) => res.arrayBuffer())
+      .then((res) => res.buffer())
       .then((buffer) => {
         fs.writeFileSync(`./output/${attachment.name}`, buffer)
       })
