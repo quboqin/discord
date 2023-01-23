594d4151cebaf418d570bde1a5ff41358b609c23
quboqin <quboqin0710@gmail.com>
2023-01-23 11:32:07 +0800

    init

---
+++ src/bot.js
@@ -0,0 +1,25 @@
+import { config as dotenv } from 'dotenv' 
+dotenv({ path: `.env` })
+
+import discord from 'discord.js'
+const { Client, GatewayIntentBits } = discord
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`bot is online`)
+})
+
+client.on("message", async (message) => {
+  console.log(message)
+  if (message.content === "ping") {
+    message.reply("pong");
+  }
+})
+
+client.login(process.env.TOKEN)
+
